daa1c5064111fdd58e7a7461a1cdab351b2a1d0e
John Peterson <jlpeterson829@gmail.com>
2015-03-23 17:24:51 -0400

    Add annotation for db models/schema

--- server/Models/dependencyModel1.js
+++ server/Models/dependencyModel1.js
@@ -1,34 +1,28 @@
 var mongoose = require('mongoose');
 var Repo = require('./repoModel.js');
 
+// create a mongoose schema for each of the library (or dependency) entries
 var DependencySchema = new mongoose.Schema({
+  //add a name property
   name: {
     type: String,
     //required: true,
     unique: true
   },
+  // add a boolean for whether or not this is a dev dependency 
   devOrNot: {
     type: Boolean,
     required: true
   },
+  // add an array of pointers to the Repo instances. These are strings with the repo name
   repos: [{
       type: mongoose.Schema.Types.String,
       ref: 'Repo'
     }]
 });
 
+// define the mongoose model 
 var Dependency = mongoose.model('Dependency1', DependencySchema, 'Dependencies');
 
-
-// var testDependency = new Dependency({
-//   name: 'testName',
-//   //devDependency: true,
-//   repos: []
-// });
-// testDependency.save(function(err,t){
-//   if (err) console.log("ERROR!");
-//   console.log("id number ", t._id);
-// });
-
-
+// Export the model
 module.exports = Dependency;
