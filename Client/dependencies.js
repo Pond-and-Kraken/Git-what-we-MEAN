f623919d47beaa70249e989e1db255d8f6e4eb79
Daniel Miller <danielmiller.nl@gmail.com>
2015-03-23 16:45:31 -0230

    comments in Client/ and Server/Routes/

--- Client/dependencies.js
+++ Client/dependencies.js
@@ -4,25 +4,18 @@ angular.module('gitDepends.dependencies', [])
 
     $scope.data = [];
     $scope.search = '';
-    console.log("In the controller");
-    //var orderBy = $filter('orderBy');
     $scope.getDependencies = function() {
       // Use Dependencies factory to get array of Dependeny objects from database
       // set $scope.dependencies to the array
       // in html file, use ng-repeat to iterate over array, display dependency name and length of repo array
 
      Dependencies.getDependencies().then(function(data) {
-        console.log("DATA ", data);
-        $scope.data = data;
-        //$scope.order('-visits',false);
+        $scope.dependencies = data;
       }).catch(function(error) {
         console.error(error);
       })
     };
     $scope.getDependencies();
-    // $scope.order = function(predicate, reverse) {
-    //   $scope.data.links = orderBy($scope.data.links, predicate, reverse);
-    // };
   }])
   .factory('Dependencies', function($http) {
     var getDependencies = function() {
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,6 @@ var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('index.js router.get');
   res.render('index');
 });
 
